7a31462724009c381bc14fde1c4643dac2284377
Huynh-Thanh-Dong <huynhthanhdong05102001@gmail.com>
2022-08-06 17:40:11 +0700

    first commit

--- src/views/budget.vue
+++ src/views/budget.vue
@@ -1,3 +1,109 @@
 <template>
-  <h1>Budget Page...</h1>
+  <div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-users text-2xl">users</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-arrow-right text-2xl">arrow-right</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-calendar text-2xl">calendar</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-camera text-2xl">camera</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-category text-2xl">category</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-chart text-2xl">chart</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-document text-2xl">document</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-filter text-2xl">filter</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-info-square text-2xl">info-square</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-location text-2xl">location</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-logout text-2xl">logout</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-notification text-2xl">notification</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-presentation text-2xl">presentation</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-profile text-2xl">profile</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-setting text-2xl">setting</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-lock text-2xl">lock</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-ticket-star text-2xl">ticket-star</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-trending-down text-2xl">trending-down</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-trending-up text-2xl">trending-up</i>
+      </span>
+    </div>
+    <div>
+      <span class="flex-none w-10 mr-4">
+        <i class="t2ico t2ico-wallet text-2xl">wallet</i>
+      </span>
+    </div>
+    <!-- <div>
+      <span> <i class"t2ico t2ico-users t2ico-arrow-right t2ico-calendar t2ico-camera t2ico-category t2ico-chart t2ico-document t2ico-filter
+t2ico-info-square t2ico-location t2ico-logout t2ico-notification t2ico-plus t2ico-presentation t2ico-profile t2ico-setting t2ico-lock t2ico-ticket-star t2ico-trending-down t2ico-trending-up t2ico-wallet
+">Budget Page...</i></span>
+</div> -->
+  </div>
 </template>
